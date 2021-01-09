1cf6be0039b6d2b460716f7296a517dd47f1c596
Horst Gutmann <zerok@zerokspot.com>
2021-01-09 19:53:32 +0100

    chore: Add goreleaser configuration

--- cmd/retoots/main.go
+++ cmd/retoots/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"os"
 
@@ -11,14 +12,22 @@ import (
 	"github.com/zerok/retoots/pkg/server"
 )
 
+var version, commit, date string
+
 func main() {
 	var addr string
 	var allowedOrigins []string
 	var allowedRootAccounts []string
+	var showVersion bool
 	pflag.StringVar(&addr, "addr", "localhost:8000", "Address to listen on for requests")
 	pflag.StringSliceVar(&allowedOrigins, "allowed-origins", []string{"localhost:8000"}, "CORS allowed origins")
 	pflag.StringSliceVar(&allowedRootAccounts, "allowed-root-accounts", []string{}, "Accounts that are allowed as root of discussions")
+	pflag.BoolVar(&showVersion, "version", false, "Show version information")
 	pflag.Parse()
+	if showVersion {
+		fmt.Printf("Version: %s\nCommit: %s\nBuild date: %s\n", version, commit, date)
+		os.Exit(0)
+	}
 	logger := zerolog.New(zerolog.ConsoleWriter{Out: os.Stderr}).With().Timestamp().Logger()
 	ctx := logger.WithContext(context.Background())
 	srv := http.Server{}
